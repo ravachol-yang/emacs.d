2e275b05eeb3682d5a6fbad773a10a44dd0bcd21
Ravachol <ravachol.yang@gmail.com>
2024-02-14 23:07:10 +0800

    Remove OSX packages

--- init.el
+++ init.el
@@ -64,7 +64,7 @@
 (require 'init-frame-hooks)
 (require 'init-xterm)
 (require 'init-themes)
-(require 'init-osx-keys)
+;; (require 'init-osx-keys)
 (require 'init-gui-frames)
 (require 'init-dired)
 (require 'init-isearch)
