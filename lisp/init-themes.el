ce84b11b55b7bdcdcd39ad7e6816e806d33f4e4f
Ravachol <ravachol.yang@gmail.com>
2024-02-18 16:10:09 +0800

    add some themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -6,8 +6,18 @@
 (add-to-list 'default-frame-alist '(height . 50))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(maybe-require-package 'sublime-themes)
-(load-theme 'brin t)
+
+;; Sublime themes
+;; (maybe-require-package 'sublime-themes)
+
+;; Dracula theme
+(maybe-require-package 'dracula-theme)
+
+;; Timu's themes
+;; (maybe-require-package 'timu-spacegrey-theme)
+(maybe-require-package 'timu-rouge-theme)
+
+(load-theme 'timu-rouge t)
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
