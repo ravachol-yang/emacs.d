1d61b9b20a27bd1bb1ce6c5f81adbb6b5314d8d0
Ravachol <ravachol.yang@gmail.com>
2024-02-26 05:46:25 +0800

    Condig Org Mode: add truncate lines to nil hook

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,7 +3,9 @@
 ;;; Code:
 
 (use-package org
-  :ensure t)
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (setq truncate-lines nil))))
 
 (provide 'init-org)
 ;;; init-org.el ends here
